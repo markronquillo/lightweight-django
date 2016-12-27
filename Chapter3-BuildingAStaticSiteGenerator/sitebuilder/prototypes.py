ef1ffc4394f22f849b32514d63eb1e2038f67437
Mark Joseph Ronquillo <markronquillo23@gmail.com>
2016-12-27 18:04:30 +0800

    Finish Chapter3: Static Builder Site

--- Chapter3-BuildingAStaticSiteGenerator/sitebuilder/prototypes.py
+++ Chapter3-BuildingAStaticSiteGenerator/sitebuilder/prototypes.py
@@ -1,6 +1,8 @@
-import sys
+import sys, os
 
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
@@ -11,10 +13,42 @@ settings.configure(
         'django.contrib.staticfiles',
         # this is deprecated in 1.8+ version
         # 'django.contrib.webdesign',
-        'sitebuilder'
+        'sitebuilder',
+        'compressor'
     ),
-    STATIC_URL='/static/',
+    STATIC_URL='/static/', 
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'sitebuilder', 'pages'),
+    SITE_OUTPUT_DIRECTORY=os.path.join(BASE_DIR, '_build'),
+    STATIC_ROOT=os.path.join(BASE_DIR, '_build', 'static'),
+    STATICFILES_FINDERS=(
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'compressor.finders.CompressorFinder'
+    ),
+
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [
+                # insert your TEMPLATE_DIRS here
+            ],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                    # list if you haven't customized them:
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 )
+
 
 
 if __name__ == "__main__":
--- Chapter3-BuildingAStaticSiteGenerator/sitebuilder/sitebuilder/urls.py
+++ Chapter3-BuildingAStaticSiteGenerator/sitebuilder/sitebuilder/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = ()
+from django.conf.urls import url
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import page
+
+urlpatterns = [
+	url(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
+	url(r'^$', page, name='homepage'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
